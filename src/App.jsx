3ee8c5deb9c3ccb030bb53d23474ee8bb8b204a3
SOCRAMBLLE <mmartins@lab4tech.lan>
2024-03-13 08:52:53 +0100

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/header";
 import "./style.css";
 
 function App() {
   return (
     <>
-      <h1>MEME GENERATOR</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header--brand">
+        <img src="./images/troll-face.png" />
+        <h2>Meme Generator</h2>
+      </div>
+      <h4 className="header--subtitle">React Course - Project 3</h4>
+    </header>
+  );
+}
