0c4e0b77f4fb290e9a075467210538f1e7457460
SOCRAMBLLE <mmartins@lab4tech.lan>
2024-03-14 11:04:05 +0100

    Added logic to form to add text to meme

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -19,13 +19,28 @@ export default function Meme() {
     }));
   }
 
+  function handleFormChange(e) {
+    const { name, value } = e.target;
+    setRandomMeme((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
         <div>
           <label className="form--label">
             Top Text
-            <input type="text" placeholder="Shut up" className="form--input" />
+            <input
+              type="text"
+              placeholder="Shut up"
+              className="form--input"
+              value={randomMeme.topText}
+              name="topText"
+              onChange={handleFormChange}
+            />
           </label>
         </div>
         <div>
@@ -35,15 +50,22 @@ export default function Meme() {
               type="text"
               placeholder="and take my money"
               className="form--input"
+              value={randomMeme.bottomText}
+              name="bottomText"
+              onChange={handleFormChange}
             />
           </label>
         </div>
-        <button className="form--button" onClick={handleClick}>
+        <button type="button" className="form--button" onClick={handleClick}>
           Get a new meme image !
         </button>
       </div>
       {randomMeme.randomImage ? (
-        <img src={randomMeme.randomImage} className="meme--img" />
+        <div className="meme">
+          <img src={randomMeme.randomImage} className="meme--img" />
+          <h2 className="meme--text top">{randomMeme.topText}</h2>
+          <h2 className="meme--text bottom">{randomMeme.bottomText}</h2>
+        </div>
       ) : (
         <h5 className="meme--default">Click the button to get a meme</h5>
       )}
